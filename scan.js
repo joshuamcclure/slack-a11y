dbea8de6796b37aa5028483be53ddaeff97544df
Joshua McClure <joshua.mcclure@deque.com>
2019-11-13 08:49:03 -0500

    Refactoring

--- scan.js
+++ scan.js
@@ -1,11 +1,14 @@
 const { scan, response, sendResponse } = require('./utils');
 
-const performScan = async (url, requestBody) => {
-    requestBody = JSON.parse(requestBody);
-    const results = await scan(url);
-    const responseBody = response(url, results);
-    // sendResponse(requestBody, responseBody);
-    console.log(responseBody);
-}
-
-performScan(process.argv[2], process.argv[3]);
+(async () => {
+    try {
+        const url = process.argv[2];
+        const requestBody = JSON.parse(process.argv[3]);
+        const results = await scan(url);
+        const responseBody = response(url, results);
+        
+        sendResponse(requestBody, responseBody);
+    } catch(e) {
+        console.log(e);
+    }
+})()
